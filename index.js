e21f69c029a3ce3f3198fe148ea18361edb5fd5e
dingledoink <webhead@gmail.com>
2025-03-22 17:16:54 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 import express from 'express';
+import { executablePath } from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('BenchApp bot is up and running!');
+  res.send('BenchApp bot is up and running with /scrape!');
+});
+
+app.get('/scrape', async (req, res) => {
+  try {
+    const browser = await puppeteer.launch({
+      args: chrome.args,
+      executablePath: await executablePath(),
+      headless: chrome.headless,
+    });
+
+    const page = await browser.newPage();
+    await page.goto('https://www.benchapp.com/schedule/list', { waitUntil: 'networkidle2' });
+    const content = await page.content();
+    await browser.close();
+
+    res.send({ html: content });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: err.message });
+  }
 });
 
 app.listen(PORT, () => {
